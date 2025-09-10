aa3c3a1178d227aaac852b026bc94ccb9608b8a9
Phạm Ngọc Phong <phongpham131102@gmail.com>
2025-09-10 14:42:07 +0700

    feat: thêm chức năng lấy quyền theo ID vai trò, cập nhật các tuyến đường và cải thiện xác thực với middleware phân quyền

--- src/routes/permission.routes.ts
+++ src/routes/permission.routes.ts
@@ -1,20 +1,23 @@
 import { Router } from 'express';
 import { PermissionController } from '../controllers/permission.controller';
 import { authenticateToken, } from '../middleware/auth.middleware';
+import { authorization } from '../middleware/authorization.middleware';
 import { validateDto } from '../middleware/validate-dto.middleware';
 import { CreatePermissionDto, UpdatePermissionDto } from '../dto/permissions/create-permission.dto';
 import { CreatePermissionRoleDto } from '../dto/permissions/create-permission-role.dto';
 import { UpdatePermissionRoleDto } from '../dto/permissions/update-permission-role.dto';
+import { ActionEnum, SubjectEnum } from '../models/permission.model';
 
 const router = Router();
 const permissionController = new PermissionController();
 
 // Protected routes (admin only)
 router.get('/', authenticateToken, (req, res) => permissionController.getAllPermissions(req, res));
+router.get('/get-all-by-role-id/:id', authenticateToken, (req, res) => permissionController.getPermissionsByRoleId(req, res));
 router.get('/role/:roleId', authenticateToken, (req, res) => permissionController.getPermissionsByRole(req, res));
 router.post('/', authenticateToken, validateDto(CreatePermissionDto), (req, res) => permissionController.createPermission(req, res));
-router.post('/create-permission-role', authenticateToken, validateDto(CreatePermissionRoleDto), (req, res) => permissionController.createPermissionRole(req, res));
-router.put('/update-permission-by-role/:id', authenticateToken, validateDto(UpdatePermissionRoleDto), (req, res) => permissionController.updatePermissionByRoleId(req, res));
+router.post('/create-permission', authenticateToken, authorization(SubjectEnum.ROLE, ActionEnum.CREATE), validateDto(CreatePermissionRoleDto), (req, res) => permissionController.createPermissionRole(req, res));
+router.put('/update-permission-by-role-id/:id', authenticateToken, validateDto(UpdatePermissionRoleDto), (req, res) => permissionController.updatePermissionsByRoleId(req, res));
 router.put('/:id', authenticateToken, validateDto(UpdatePermissionDto), (req, res) => permissionController.updatePermission(req, res));
 router.delete('/:id', authenticateToken, (req, res) => permissionController.deletePermission(req, res));
 
